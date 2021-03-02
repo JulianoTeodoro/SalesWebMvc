c95759f5769d31423c3270947a742b25cdf8d6c5
Juliano Teodoro <julianoteodoro2002@gmail.com>
2021-03-02 20:50:07 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -10,8 +10,28 @@ namespace SalesWebMvc.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
 
+        }
+
+        public Department(int iD, string name)
+        {
+            ID = iD;
+            Name = name;
+        }
+
+        public void addSeller(Seller sr)
+        {
+            Sellers.Add(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.totalSales(initial, final));
+        }
 
     }
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus status { get; set; }
+
+        public SalesRecord(int iD, DateTime date, double amount, SaleStatus status)
+        {
+            ID = iD;
+            Date = date;
+            Amount = amount;
+            this.status = status;
+        }
+    }
+}
